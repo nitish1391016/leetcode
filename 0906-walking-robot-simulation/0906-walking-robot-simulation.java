dd2d4110a89a5ef6adcb9317f3421a73d8331627
nitish pala <80934407+nitish1391016@users.noreply.github.com>
2024-09-04 20:22:34 +0530

    Time: 12 ms (95.63%), Space: 48.9 MB (85.59%) - LeetHub

--- 0906-walking-robot-simulation/0906-walking-robot-simulation.java
+++ 0906-walking-robot-simulation/0906-walking-robot-simulation.java
@@ -1,21 +1,34 @@
 class Solution {
     public int robotSim(int[] commands, int[][] obstacles) {
-        HashSet<String> set = new HashSet<>();
-        for(int[] o: obstacles){
-            set.add(o[0]+" "+o[1]);
+        HashSet<Integer> set = new HashSet<>();
+        for (int[] o : obstacles) {
+            set.add(o[0] * 40001 + o[1]);
         }
-        int[][] dirs = new int[][] {{0,1}, {1,0}, {0, -1}, {-1, 0}};
-        int x = 0, y =0;
+
+        int[][] dirs = new int[][] { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+        int x = 0, y = 0;
         int d = 0;
         int result = 0;
-        for(int command: commands){
-            d = (command == -1 && ++d == 4) ? 0: (command == -2 && --d == -1) ? 3 : d;
-            while(d >= 0 && command-- > 0 && !set.contains((x+dirs[d][0])+" "+(y+dirs[d][1]))){
-                x+= dirs[d][0];
-                y+= dirs[d][1];
-                result = Math.max(result, (x*x+y*y));
+
+        for (int command : commands) {
+            if (command == -1) {
+                d = (d + 1) % 4;
+            } else if (command == -2) {
+                d = (d + 3) % 4;
+            } else {
+                while (command-- > 0) {
+                    int nx = x + dirs[d][0];
+                    int ny = y + dirs[d][1];
+                    if (set.contains(nx * 40001 + ny)) {
+                        break;
+                    }
+                    x = nx;
+                    y = ny;
+                    result = Math.max(result, x * x + y * y);
+                }
             }
         }
+
         return result;
     }
 }
