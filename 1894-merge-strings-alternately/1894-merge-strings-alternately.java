c573a789a7cac792c039f37c7de644a1c35f336a
nitish pala <80934407+nitish1391016@users.noreply.github.com>
2024-09-08 10:24:29 +0530

    Time: 8 ms (8.08%), Space: 42.6 MB (23.73%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.java
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -1,24 +1,18 @@
 class Solution {
-    private String returnRes(String str1, String str2, boolean flag){
-        String res = "";
-        int i =0, j=0;
-        while(j<str2.length()){
-            if(flag){
-                res+=str1.charAt(i++);
-                flag= false;
-            }
-            else{
-                res+=str2.charAt(j++);
-                flag = true;
-            }
-        }
-        res+=str1.substring(i,str1.length());
-        return res;
-    }
     public String mergeAlternately(String word1, String word2) {
-        if(word1.length()<word2.length())
-            return returnRes(word2, word1, false);
-        return returnRes(word1, word2, true);
-        
+        int i = 0;
+        int m = word1.length();
+        int n = word2.length();
+        String res = "";
+        while (i < m && i < n) {
+            res += word1.charAt(i);
+            res += word2.charAt(i++);
+        }
+        while (i < m) {
+            res += word1.charAt(i++);
+        }
+        while (i < n)
+            res += word2.charAt(i++);
+        return res;
     }
 }
