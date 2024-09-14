c97944d4bae308bee875bd88d0539631824e43f7
nitish pala <80934407+nitish1391016@users.noreply.github.com>
2024-09-14 12:49:56 +0530

    Time: 4 ms (30.56%), Space: 58.4 MB (24.05%) - LeetHub

--- 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.java
+++ 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.java
@@ -1,17 +1,15 @@
 class Solution {
     public int[] xorQueries(int[] arr, int[][] q) {
-        int[] xor = new int[arr.length];
-        xor[0] = arr[0];
         for(int i=1;i<arr.length;i++){
-            xor[i] = arr[i]^xor[i-1];
+            arr[i] = arr[i-1]^arr[i];
         }
         
         int[] res = new int[q.length];
         for(int i=0;i<q.length;i++){
             if(q[i][0] > 0)
-                res[i] = xor[q[i][0]-1]^xor[q[i][1]];
+                res[i] = arr[q[i][0]-1]^arr[q[i][1]];
             else
-                res[i] = xor[q[i][1]];
+                res[i] = arr[q[i][1]];
         }
         return res;
     }
