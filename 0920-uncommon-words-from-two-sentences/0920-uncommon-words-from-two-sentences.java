9c161015080cdf276e570fa864ab69e77a53e141
nitish pala <80934407+nitish1391016@users.noreply.github.com>
2024-09-17 12:35:44 +0530

    Time: 6 ms (11.94%), Space: 42.3 MB (22.88%) - LeetHub

--- 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.java
+++ 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.java
@@ -1,33 +1,11 @@
 class Solution {
-    public String[] uncommonFromSentences(String s1, String s2) {
-        String[] arr1 = s1.split(" ");
-        String[] arr2 = s2.split(" ");
-        HashMap<String, Integer> map1 = new HashMap<>();
-        HashMap<String, Integer> map2 = new HashMap<>();
-
-        for(String str: arr1)
-            map1.put(str, map1.getOrDefault(str, 0)+1);
-        for(String str: arr2)
-            map2.put(str, map2.getOrDefault(str, 0)+1);
-
-
-        List<String> res = new ArrayList<>();
-        for(var entry : map1.entrySet()){
-            String word = entry.getKey();
-            int wordFreq = entry.getValue();
-            if(wordFreq == 1 && !map2.containsKey(word)){
-                 res.add(word);
+    public String[] uncommonFromSentences(String A, String B) {
+         Set<String> distinct = new HashSet<>(), com = new HashSet<>();
+        for (String s : (A + " " + B).split("\\s")) {
+            if (com.contains(s) || !distinct.add(s)) { 
+                distinct.remove(s); com.add(s); 
             }
         }
-       
-        for(var entry : map2.entrySet()){
-            String word = entry.getKey();
-            int wordFreq = entry.getValue();
-            if(wordFreq == 1 && !map1.containsKey(word)){
-                 res.add(word);
-            }
-        }
-
-        return res.stream().toArray(String[]::new);
+        return distinct.toArray(new String[0]);
     }
 }
