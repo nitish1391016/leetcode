638e9410237f7d9286198efe5d28f811d485487d
nitish pala <80934407+nitish1391016@users.noreply.github.com>
2024-10-02 10:13:38 +0530

    Time: 52 ms (14.46%), Space: 62.8 MB (14.71%) - LeetHub

--- 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.java
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.java
@@ -1,21 +1,25 @@
+class ValueIndex {
+    int val;
+    int index;
+
+    public ValueIndex(int val, int index) {
+        this.val = val;
+        this.index = index;
+    }
+}
+
 class Solution {
     public int[] arrayRankTransform(int[] arr) {
-        TreeMap<Integer, List<Integer>> map = new TreeMap<Integer, List<Integer>>();
-        for(int i=0;i<arr.length;i++){
-            if(map.containsKey(arr[i])){
-                map.get(arr[i]).add(i);
-            }
-            else{
-                map.put(arr[i], new LinkedList<Integer>());
-                map.get(arr[i]).add(i);
-            }
-        }
-        int rank =1;
-        for(int key: map.keySet()){
-            for(int index: map.get(key)){
-                arr[index] = rank;
-            }
-            rank++;
+        PriorityQueue<ValueIndex> pq = new PriorityQueue<>((a, b) -> a.val - b.val);
+        for (int i = 0; i < arr.length; i++)
+            pq.add(new ValueIndex(arr[i], i));
+        int rank = 0, prev = Integer.MIN_VALUE;
+        while (!pq.isEmpty()) {
+            ValueIndex temp = pq.poll();
+            if (temp.val > prev)
+                rank++;
+            arr[temp.index] = rank;
+            prev = temp.val;
         }
         return arr;
     }
