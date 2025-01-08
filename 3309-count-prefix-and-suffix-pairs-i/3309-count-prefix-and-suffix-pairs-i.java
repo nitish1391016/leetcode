5a10222b28c6f211c33f8725024c1d988d8f3a03
nitish pala <80934407+nitish1391016@users.noreply.github.com>
2025-01-08 09:01:28 +0530

    Time: 125 ms (5.56%), Space: 45 MB (5.56%) - LeetHub

---
+++ 3309-count-prefix-and-suffix-pairs-i/3309-count-prefix-and-suffix-pairs-i.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isIndexPairs(String str1, String str2){
+        if(str1.length() > str2.length()) return false;
+        else if(str1.length() == str2.length())
+            return str1.equals(str2)? true: false; 
+        System.out.println(str2.substring(0, str1.length()));
+        System.out.println(str2.substring(str2.length()-str1.length()-1));
+        return str2.substring(0, str1.length()).equals(str1) && str2.substring(str2.length()-str1.length()).equals(str1) ? true: false;
+        
+    }
+
+    public int countPrefixSuffixPairs(String[] words) {
+        int res =0;
+        for(int i=0;i<words.length;i++){
+            for(int j=i+1;j<words.length;j++){
+                if(isIndexPairs(words[i], words[j])){
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+}
