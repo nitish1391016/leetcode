ac206af1f9d12b956a74003d5024c09482ff5aec
nitish pala <80934407+nitish1391016@users.noreply.github.com>
2025-04-20 22:21:50 +0530

    Time: 4 ms (30.84%), Space: 42.3 MB (56.02%) - LeetHub

--- 0797-rabbits-in-forest/0797-rabbits-in-forest.java
+++ 0797-rabbits-in-forest/0797-rabbits-in-forest.java
@@ -1,11 +1,27 @@
 class Solution {
-        public int numRabbits(int[] answers) {
-        Map<Integer, Integer> m = new HashMap<>();
-        for (int i : answers)
-            m.put(i, m.getOrDefault(i, 0) + 1);
-        int res = 0;
-        for (int i : m.keySet())
-            res += (m.get(i) + i) / (i + 1) * (i + 1);
-        return res;
+    public int numRabbits(int[] answers) {
+        HashMap<Integer, Integer> groupFreq = new HashMap<>();
+        int count = 0;
+        
+        for (int ele : answers) {
+            if (ele == 0) {
+                count += 1;
+            } else {
+                groupFreq.put(ele, groupFreq.getOrDefault(ele, 0) + 1);
+                if (groupFreq.get(ele) == ele + 1) {
+                    count += ele + 1;
+                    groupFreq.put(ele, 0);
+                }
+            }
+        }
+        
+        for (int key : groupFreq.keySet()) {
+            int freq = groupFreq.get(key);
+            if (freq > 0) {
+                count += key + 1;
+            }
+        }
+        
+        return count;
     }
 }
