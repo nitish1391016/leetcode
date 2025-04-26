15dbd932fad9244c304f343cef2d708e990c4e90
nitish pala <80934407+nitish1391016@users.noreply.github.com>
2025-04-26 13:47:44 +0530

    Time: 8 ms (54.6%), Space: 60 MB (92.58%) - LeetHub

--- 2527-count-subarrays-with-fixed-bounds/2527-count-subarrays-with-fixed-bounds.java
+++ 2527-count-subarrays-with-fixed-bounds/2527-count-subarrays-with-fixed-bounds.java
@@ -1,24 +1,12 @@
 class Solution {
-    public long countSubarrays(int[] nums, int minK, int maxK) {
-        long res = 0;
-        int badIdx = -1, leftIdx = -1, rightIdx = -1;
-
-        for (int i = 0; i < nums.length; ++i) {
-            if (!(minK <= nums[i] && nums[i] <= maxK)) {
-                badIdx = i;
-            }
-
-            if (nums[i] == minK) {
-                leftIdx = i;
-            }
-
-            if (nums[i] == maxK) {
-                rightIdx = i;
-            }
-
-            res += Math.max(0, Math.min(leftIdx, rightIdx) - badIdx);
+        public long countSubarrays(int[] A, int minK, int maxK) {
+        long res = 0, jbad = -1, jmin = -1, jmax = -1, n = A.length;
+        for (int i = 0; i < n; ++i) {
+            if (A[i] < minK || A[i] > maxK) jbad = i;
+            if (A[i] == minK) jmin = i;
+            if (A[i] == maxK) jmax = i;
+            res += Math.max(0L, Math.min(jmin, jmax) - jbad);
         }
-
         return res;
     }
 }
