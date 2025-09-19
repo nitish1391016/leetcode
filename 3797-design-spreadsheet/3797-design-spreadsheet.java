abd1e130196fa9bb66983dc77e20bca2f581a994
nitish pala <80934407+nitish1391016@users.noreply.github.com>
2025-09-19 23:12:54 +0530

    Time: 117 ms (69.01%), Space: 56.7 MB (13.03%) - LeetHub

--- 3797-design-spreadsheet/3797-design-spreadsheet.java
+++ 3797-design-spreadsheet/3797-design-spreadsheet.java
@@ -1,41 +1,26 @@
 class Spreadsheet {
-    private int[][] sheet;
+    private HashMap<String, Integer> map;
+
     public Spreadsheet(int rows) {
-        sheet = new int[28][rows+2];
+        map = new HashMap<String, Integer>();
+        
     }
     
     public void setCell(String cell, int value) {
-        int x = (int)(cell.charAt(0)-'A');
-        int y = Integer.parseInt(cell.substring(1))-1;
-        sheet[x][y] = value;
+        map.put(cell, value);
     }
     
     public void resetCell(String cell) {
-        int x = (int)(cell.charAt(0)-'A');
-        int y = Integer.parseInt(cell.substring(1))-1;
-        sheet[x][y] = 0;        
+        map.put(cell, 0);
     }
     
     public int getValue(String formula) {
-        formula = formula.substring(1);
-        String[] split = formula.split("\\+", 2); 
-        int num1=0, num2=0;
-        if(split[0].charAt(0) >= 'A' && split[0].charAt(0) <= 'Z') {
-            num1 = getCell(split[0]);
-        }
-        else
-            num1 = Integer.parseInt(split[0]);
-        if(split[1].charAt(0) >= 'A' && split[1].charAt(0) <= 'Z') {
-            num2 = getCell(split[1]);
-        }
-        else
-            num2 = Integer.parseInt(split[1]);
-        return num1+num2;
-    }
-    private int getCell(String cell){
-        int x = (int)(cell.charAt(0)-'A');
-        int y = Integer.parseInt(cell.substring(1))-1;
-        return sheet[x][y];
+        String[] splits = formula.substring(1).split("\\+", 2);
+        String left = splits[0];
+        String right = splits[1];
+        int left1 = Character.isUpperCase(left.charAt(0)) ? map.getOrDefault(left, 0): Integer.parseInt(left);
+        int right2 = Character.isUpperCase(right.charAt(0)) ? map.getOrDefault(right, 0): Integer.parseInt(right);
+        return left1+right2;
     }
 }
 
