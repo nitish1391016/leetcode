4c6b78bb12244be866f7e1c8bb0edd848233e3ca
nitish pala <80934407+nitish1391016@users.noreply.github.com>
2025-09-25 07:51:40 +0530

    Time: 8 ms (31.05%), Space: 45.8 MB (20.13%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.java
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.java
@@ -2,25 +2,27 @@ class Solution {
     public int minFallingPathSum(int[][] m) {
         int len = m.length;
         int[][] dp = new int[len][len];
-        
+        for(int row = len-1; row>=0; row--){
+            for(int col = len-1;col >=0; col--){
+                if(row == len-1){
+                    dp[row][col] = m[row][col];
+                }
+                else{
+                    int right = Integer.MAX_VALUE;
+                    int left = Integer.MAX_VALUE;
+                    if(col-1 >=0)
+                        left = dp[row+1][col-1];
+                    if(col+1 < len)
+                        right = dp[row+1][col+1];
+                    int centre = dp[row+1][col];
+                    dp[row][col] = m[row][col]+ Math.min(left, Math.min(right, centre));
+                }
+            }
+        }
         int min = Integer.MAX_VALUE;
-        for(int[] list: dp)
-            Arrays.fill(list, Integer.MAX_VALUE);
-        for(int i=0;i < len; i++){
-            min = Math.min(min, minFalling(m, 0, i, dp));
+        for(int i=0;i<len;i++){
+            min = Math.min(min, dp[0][i]);
         }
         return min;
-    }
-
-    public int minFalling(int[][] m, int row, int col, int[][] dp){
-        if(row == m.length || col < 0 || col == m.length) return Integer.MAX_VALUE;
-        if(row == m.length-1 && col < m.length) return m[row][col];
-        if(dp[row][col] != Integer.MAX_VALUE)
-            return dp[row][col];
-        int left = minFalling(m, row+1, col-1, dp); 
-        int right = minFalling(m, row+1, col+1, dp);
-        int centre = minFalling(m, row+1, col, dp);
-        dp[row][col] = m[row][col] + Math.min(left, Math.min(right, centre));
-        return dp[row][col];
     }
 }
