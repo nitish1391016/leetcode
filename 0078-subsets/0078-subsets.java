4afcc0e61bd66f4ea1ff779e666dff6540372496
nitish pala <80934407+nitish1391016@users.noreply.github.com>
2025-09-25 22:23:07 +0530

    Time: 1 ms (48.33%), Space: 42.9 MB (46.93%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,12 +1,12 @@
 class Solution {
     List<List<Integer>> res = new LinkedList<List<Integer>>();
     public List<List<Integer>> subsets(int[] nums) {
-        subset(nums, new LinkedList<Integer>(), nums.length-1);
+        subset(nums, new ArrayList<Integer>(), nums.length-1);
         return res;
     }
     public void subset(int[] nums, List<Integer> sub, int n){
         if(n == -1) {
-            res.add(new LinkedList(sub));
+            res.add(new ArrayList(sub));
             return;
         }
         sub.add(nums[n]);
