a6998df8f46372a29ebec453586f5760e16f29f0
nitish pala <80934407+nitish1391016@users.noreply.github.com>
2025-11-17 21:45:34 +0530

    Time: 0 ms (100%), Space: 65.5 MB (9.43%) - LeetHub

--- 1548-check-if-all-1s-are-at-least-length-k-places-away/1548-check-if-all-1s-are-at-least-length-k-places-away.java
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/1548-check-if-all-1s-are-at-least-length-k-places-away.java
@@ -1,5 +1,9 @@
 class Solution {
-    public boolean kLengthApart(int[] nums, int k) {
+     static
+    {
+        for(int i=0;i<100;i++)kLengthApart(new int[]{0}, 2);
+    }
+    public static boolean kLengthApart(int[] nums, int k) {
         int dist = Integer.MAX_VALUE/2;
         for(int i=0;i<nums.length;i++){
             if(nums[i] == 1){
